3a57ade572606d39ed0b58b24868f7f61c0b275b
3018 <1330133018@qq.com>
2017-09-12 15:32:36 +0800

    第一次提交

---
+++ src/main/java/org/lanqiao/dao/StudentDao.java
@@ -0,0 +1,17 @@
+package org.lanqiao.dao;
+
+import org.lanqiao.entity.Student;
+
+public interface StudentDao {
+    int deleteByPrimaryKey(Integer sid);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    Student selectByPrimaryKey(Integer sid);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
+}
