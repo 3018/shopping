e7aee071f1aa9dad2d8b29a37042a6997ff40af1
3018 <1330133018@qq.com>
2017-09-12 15:41:18 +0800

    第二次提交

---
+++ src/main/java/org/lanqiao/dao/TeacherDao.java
@@ -0,0 +1,17 @@
+package org.lanqiao.dao;
+
+import org.lanqiao.entity.Teacher;
+
+public interface TeacherDao {
+    int deleteByPrimaryKey(Integer tid);
+
+    int insert(Teacher record);
+
+    int insertSelective(Teacher record);
+
+    Teacher selectByPrimaryKey(Integer tid);
+
+    int updateByPrimaryKeySelective(Teacher record);
+
+    int updateByPrimaryKey(Teacher record);
+}
